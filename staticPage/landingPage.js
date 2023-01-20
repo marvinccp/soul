d45f4e27c1d430fe0437b730530c56ba2eef5cac
Marvin Berrio <marvinccp@gmail.com>
2023-01-21 00:01:22 +0100

    Basic responsive menu ok

---
+++ staticPage/landingPage.js
@@ -0,0 +1,8 @@
+const hamburgerItem = document.querySelector(".hamburguer-item");
+hamburgerItem.addEventListener("click", () => {
+
+
+  const navBar = document.querySelector('.nav-bar')
+  navBar.classList.toggle('active')
+
+});
