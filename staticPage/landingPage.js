97cd7413520524ae2bfaa8ba410fcd3d17042ca2
Marvin Berrio <marvinccp@gmail.com>
2023-01-21 01:40:02 +0100

    Basic routing 50%

--- staticPage/landingPage.js
+++ staticPage/landingPage.js
@@ -1,24 +1,30 @@
 const hamburgerItem = document.querySelector(".hamburguer-item");
 hamburgerItem.addEventListener("click", () => {
+  const navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active");
+});
 
-
-  const navBar = document.querySelector('.nav-bar')
-  navBar.classList.toggle('active')
-
+const about = document.querySelector(".about");
+about.addEventListener("click", () => {
+  location.href = "./about.html";
 });
 
 
-const about = document.querySelector('.about')
-about.addEventListener('click', () =>{
+const home = document.querySelector(".home");
+home.addEventListener("click", () => {
+  location.href = "./landingPage.html";
+});
 
-    let text = document.getElementById('text');
-    text.classList.add('active')
+const design = document.querySelector(".design");
+design.addEventListener("click", () => {
+  location.href = "./design.html";
+});
 
-    location.href = './about.html'
-
-})
-
-const home = document.querySelector('.home')
-home.addEventListener('click', () =>{
-    location.href = "./landingPage.html";
-})
+const contact = document.querySelector(".contact");
+contact.addEventListener("click", () => {
+  location.href = "./contact.html";
+});
+const title = document.querySelector(".title");
+title.addEventListener("click", () => {
+  location.href = "./landingPage.html";
+});
