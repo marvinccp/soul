636f3da893ab5fdc98c59b2e438ae7150467133b
Marvin Berrio <marvinccp@gmail.com>
2023-01-22 00:30:52 +0100

    Basic and simple Routing

--- staticPage/scripts/index.js
+++ staticPage/scripts/index.js
@@ -6,25 +6,27 @@ hamburgerItem.addEventListener("click", () => {
 
 const about = document.querySelector(".about");
 about.addEventListener("click", () => {
-  location.href = "./about.html";
+  location.href = "../pages/about.html"
 });
-
 
 const home = document.querySelector(".home");
 home.addEventListener("click", () => {
-  location.href = "./landingPage.html";
+  location.href = "../pages/index.html"
 });
 
 const design = document.querySelector(".design");
 design.addEventListener("click", () => {
-  location.href = "./design.html";
+  location.href = "../pages/design.html"
 });
 
 const contact = document.querySelector(".contact");
 contact.addEventListener("click", () => {
-  location.href = "./contact.html";
+  location.href = "../pages/contact.html"
 });
+
 const title = document.querySelector(".title");
 title.addEventListener("click", () => {
-  location.href = "./landingPage.html";
+  location.href = "../public/index.html"
 });
+
+
